a9025c55b478aad91a4c60a871464011bc7e6359
RalphMerling <ralphmerling@gmail.com>
2026-06-02 19:21:36 -0300

    Dia 2/6

--- src/app/routes/StackRoutes.jsx
+++ src/app/routes/StackRoutes.jsx
@@ -1,28 +1,26 @@
 //Arquivo que iremos montar as nossas rotas e configurar nossas telas
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GuiasManaus from "../screens/GuiasManaus";
 import LoginScreen from "../screens/LoginScreen";
-import RegisterAll from "../screens/RegisterAll";   
+import RegisterAll from "../screens/RegisterAll";
 import RegisterScreen from "../screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
-
 
 // A ordem das telas vai ser a ordem que elas vão aparecer, ou seja,
 // a primeira tela é a de login, depois a de registro geral e por último a de registro
 // específico
 
 // Mas também pode definir a rota inicial independente da ordem,
-// usando a propriedade initialRouteName no Stack.Navigator 
+// usando a propriedade initialRouteName no Stack.Navigator
 // (ex: <Stack.Navigator initialRouteName="register">)
 export default function StackRoutes() {
   return (
-    <Stack.Navigator initialRouteName="register">
+    <Stack.Navigator initialRouteName="guiasmanaus">
       <Stack.Screen name="login" component={LoginScreen} />
-      <Stack.Screen name="registerall" component={RegisterAll} />
       <Stack.Screen name="register" component={RegisterScreen} />
+      <Stack.Screen name="registerall" component={RegisterAll} />
+      <Stack.Screen name="guiasmanaus" component={GuiasManaus} />
     </Stack.Navigator>
-
-    //teste de commit
-
   );
 }
